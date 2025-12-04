e58a1c713500e44f93cb6e716f541c56e7d0e5a7
Akshay Antony <romeoantony1999@gmail.com>
2025-12-04 21:55:25 +0530

    Hardcode production API URL to fix connection issue

--- GreenHaven.UI/src/api/axios.js
+++ GreenHaven.UI/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const prodUrl = 'https://greenhaven-api-gfevg6efa5ghayde.southeastasia-01.azurewebsites.net/api';
+const localUrl = 'http://localhost:5177/api'; // Or import.meta.env.VITE_API_URL if you prefer
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: isLocal ? localUrl : prodUrl,
   headers: {
     'Content-Type': 'application/json',
   },
